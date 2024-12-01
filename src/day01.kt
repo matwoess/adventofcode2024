c25f22e21cde357f0f49a0fffb0b243234ced8dc
Mathias Wöß <mat.woess@gmail.com>
2024-12-01 16:22:37 +0100

    Initial Commit: - day 1 solution in Kotlin

---
+++ src/day01.kt
@@ -0,0 +1,48 @@
+package day01
+
+import java.io.File
+import kotlin.math.abs
+
+
+fun preProcessData(lines: List<String>): Pair<List<Int>, List<Int>> {
+    val list1 = mutableListOf<Int>()
+    val list2 = mutableListOf<Int>()
+    for (line in lines) {
+        val numbers = line.split("   ")
+        list1.add(numbers[0].toInt())
+        list2.add(numbers[1].toInt())
+    }
+    return Pair(list1, list2)
+}
+
+fun part1(data: Pair<List<Int>, List<Int>>): Int {
+    val list1 = data.first.sorted()
+    val list2 = data.second.sorted()
+    var totalDifference = 0
+    for (i in list1.indices) {
+        val difference = abs(list1[i] - list2[i])
+        totalDifference += difference
+    }
+    return totalDifference
+}
+
+fun part2(data: Pair<List<Int>, List<Int>>): Int {
+    val list1 = data.first.sorted()
+    val list2 = data.second.sorted()
+    var similarityScore = 0
+    for (num in list1) {
+        val occurrences = list2.count { it == num }
+        similarityScore += num * occurrences
+    }
+    return similarityScore
+}
+
+
+fun main() {
+    val input = File("inputs/day01.txt").readLines()
+    val data = preProcessData(input)
+    val answer1 = part1(data)
+    println("Answer 1: $answer1") // 1580061
+    val answer2 = part2(data)
+    println("Answer 2: $answer2") // 23046913
+}
